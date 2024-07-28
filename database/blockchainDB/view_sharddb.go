961f70f7f8b9f5ab5c2b86ded8a88caa05d09478
paul snow <snow.paul@gmail.com>
2024-07-28 11:36:36 -0500

    added view files left out from previous commits

---
+++ database/blockchainDB/view_sharddb.go
@@ -0,0 +1,206 @@
+package blockchainDB
+
+import (
+	"fmt"
+	"time"
+)
+
+// View implementation
+// Simplifying assumptions of this implementation include:
+//   - Most DB accesses will not be done within the context of a view
+//   - Views will collect until all views timeout, then they can all be cleared
+//   - Using a view can reasonably timeout in 30 seconds.  Every access
+//     can reset the timeout, so a view can stay open as long as it is
+//     actively used
+//   - Anytime the DB is accessed, we can check to see if we need to
+//     clear the views and flush data to disk
+//   - Currently rebuilding the DB after an unclean close of the DB is
+//     not supported
+
+// View Struct
+// Views allow one to grab a point in time in the database, and query values that were
+// in the DB at that time (writes after creating a view do not impact the view)
+type View struct {
+	ID         int                 // ID of a view
+	KeyValues  map[[32]byte][]byte // key value pairs
+	LastAccess time.Time           // time that the view was created
+	Closed     bool                // true if the View is closed
+}
+
+// ShardDBViews
+// A wrapper around a sharded DB that implements Views.  Views are created in a stack.
+// More recent views lookup values in their cache, and then in the views that come
+// before.  Later views are ignored.
+type ShardDBViews struct {
+	DB          *ShardDB      // The underlying DB
+	ViewID      int           // The next ViewID
+	ActiveViews []*View       // List of all active Views, newest first
+	Map         map[int]View  // Fast lookup of a view
+	Timeout     time.Duration // How long before views timeout; every access resets timeout
+}
+
+func NewShardDBViews(
+	Directory string,
+	Timeout time.Duration,
+	Partition, ShardCnt,
+	BufferCnt int) (sdbV *ShardDBViews, err error) {
+
+	sdbV = new(ShardDBViews)
+	sdbV.Timeout = Timeout
+	if sdbV.DB, err = NewShardDB(Directory, Partition, ShardCnt); err == nil {
+		return sdbV, nil
+	}
+	return nil, err
+}
+
+func OpenShardDBViews(
+	Directory string,
+	Timeout time.Duration,
+	Partition,
+	BufferCnt int) (sdbV *ShardDBViews, err error) {
+
+	sdbV = new(ShardDBViews)
+	sdbV.Timeout = Timeout
+	if sdbV.DB, err = OpenShardDB(
+		Directory,
+		Partition); err == nil {
+		return sdbV, nil
+	}
+	return nil, err
+}
+
+func (s *ShardDBViews) Close() {
+	s.ActiveViews = nil
+	s.Map = nil
+	s.DB.Close()
+}
+
+// Active Views
+// Returns true if a valid active view exists.  If old views
+// exist, but none are active, the active views are tossed.
+func (s *ShardDBViews) IsViewActive() bool {
+	// The first entry in the ActiveViews is the View Cache
+	// ActiveViews with a length less than two means no active views
+	if len(s.ActiveViews) < 2 {
+		return false
+	}
+
+	s.GetViewIndex(s.ActiveViews[1]) // This will clear ActiveViews if none are valid
+	return len(s.ActiveViews) > 0    // If any remain, then a View is Active
+}
+
+func (s *ShardDBViews) Put(key [32]byte, value []byte) error {
+
+	// If not view is active, then write to the DB
+	if !s.IsViewActive() {
+		return s.DB.Put(key, value)
+	}
+
+	s.ActiveViews[len(s.ActiveViews)-1].KeyValues[key] = value // Put key/value in last view
+	s.ActiveViews[0].KeyValues[key] = value                    // And put the key value in the cache
+	return nil
+}
+
+func (s *ShardDBViews) Get(key [32]byte) (value []byte) {
+
+	// If no view is active, just get the DB value
+	if !s.IsViewActive() {
+		return s.DB.Get(key)
+	}
+
+	// If a view is active, we need to check the view cache first
+	value, ok := s.ActiveViews[0].KeyValues[key]
+	if ok {
+		return value
+	}
+
+	return s.DB.Get(key) // Nothing in the view cache? Pull from the DB
+}
+
+func (s *ShardDBViews) NewView() *View {
+	// If no view is active, we have to cache DB updates so
+	// create a "DB Update View" at s.ActiveViews[0]
+	if !s.IsViewActive() {
+		view := new(View)
+		view.ID = 0
+		view.KeyValues = make(map[[32]byte][]byte)
+		s.ActiveViews = append(s.ActiveViews, view)
+	}
+
+	// Create a view and added it to the ActiveViews slice. Add
+	// the newest views to the end of the list.
+	view := new(View)
+	s.ViewID++
+	view.ID = s.ViewID
+	view.LastAccess = time.Now()
+	view.KeyValues = make(map[[32]byte][]byte)
+	s.ActiveViews = append(s.ActiveViews, view)
+	return view
+}
+
+// GetViewIndex
+// Returns the view index for a view.  Returns 0 if no valid view exists
+func (s *ShardDBViews) GetViewIndex(view *View) int {
+	if len(s.ActiveViews) == 0 {
+		return 0
+	}
+
+	// Look for and mark all the views that have timed out
+	for _, v := range s.ActiveViews[1:] {
+		if v.Closed {
+			return 0
+		}
+		if dt := time.Since(view.LastAccess); dt > s.Timeout {
+			v.Closed = true
+		}
+	}
+
+	// First clear ActiveViews if no active view exists
+	if len(s.ActiveViews) == 2 && s.ActiveViews[1].Closed { // Clear ActiveViews if none exist
+		s.ActiveViews = s.ActiveViews[:0]
+		return 0
+	}
+	for s.ActiveViews[1].Closed { // While the oldest ActiveView is closed, delete it
+		n := len(s.ActiveViews)
+		if n <= 2 { // If Removing the last view, clear ActiveViews
+			s.ActiveViews = s.ActiveViews[:0] // No index exists for the View
+			return 0
+		}
+		copy(s.ActiveViews[1:], s.ActiveViews[2:]) // Remove the closed View
+		s.ActiveViews = s.ActiveViews[:n-1]
+	}
+
+	for i, v := range s.ActiveViews[1:] { // Look for the view in the Views that remain
+		if v.ID == view.ID {
+			return i + 1
+		}
+	}
+
+	return 0
+}
+
+// ViewGet
+// To a get of a key value pair using a view.  The view is searched first, and all
+// active views that were created before this view are searched in turn.  If no
+// key value pair is found in the view or older views, then return what the DB has
+func (s *ShardDBViews) ViewGet(view *View, key [32]byte) (value []byte, err error) {
+	//
+	view.LastAccess = time.Now()
+	// Check if the view provided is active.  If not, return an error that the
+	// view has expired
+	viewIdx := s.GetViewIndex(view)
+	if viewIdx == 0 {
+		return nil, fmt.Errorf("view invalid")
+	}
+
+	// Check the view and all the older views for a key value pair.  Note that even
+	// if an older view has expired, we still need its key value pair rather than
+	// the current key value pair
+	for viewIdx--; viewIdx > 0; viewIdx-- {
+		if v, ok := s.ActiveViews[viewIdx].KeyValues[key]; ok {
+			return v, nil
+		}
+	}
+
+	return s.DB.Get(key), nil // If no key value pair exists, return whatever the DB has.
+}
---
+++ database/blockchainDB/view_sharddb_test.go
@@ -0,0 +1,99 @@
+package blockchainDB
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestView(t *testing.T) {
+
+	NumKeys := 1000          // Number of keys used in the test
+	Timeout := time.Second * 1 // The timeout period
+
+	Directory, rm := MakeDir()
+	defer rm()
+	sdbv, err := NewShardDBViews(Directory, Timeout, 1, 256, 1)
+	assert.NoError(t, err, "failed to open ShardDBViews")
+
+	// Collect NumKeys number of key/values, and populate the DB
+	fr := NewFastRandom([]byte{1, 2, 3})
+	keys := make(map[[32]byte][]byte) // A set of keys and their original values
+	for i := 0; i < NumKeys; i++ {
+		key := fr.NextHash()
+		value := fr.RandBuff(10, 20)
+		keys[key] = value
+		sdbv.Put(key, value)
+	}
+
+	// Make all the key/values collected are in fact in the DB
+	for k, v := range keys {
+		value := sdbv.Get(k)
+		assert.Equal(t, v, value, "Failed to get our value back")
+	}
+
+	// Create a series of views, and make sure they all see that the key
+	// value pairs are unchanged from the point the view was created.
+	// After we are done, we make sure all the views timeout and go away.
+	for i := 0; i < 10; i++ {
+		fmt.Printf("%4d AV %4d \n", i, len(sdbv.ActiveViews))
+		// collect the current state of keys
+		for k := range keys {
+			keys[k] = sdbv.Get(k)
+		}
+
+		// Modify the some of the keys, and collect the updated values
+		// This should not impact the view
+		updated := make(map[[32]byte][]byte) // make a list of updated keys
+		view := sdbv.NewView()               // Create a view
+		for k := range keys {                //                Modify 30 percent or so of the keys
+			if fr.UintN(10) < 30 {
+				fr.RandBuff(10, 20)
+				value := fr.RandBuff(10, 20)
+				updated[k] = value
+				sdbv.Put(k, value)
+			}
+		}
+
+		// check that none of the values changed in the view even if the
+		// database have been updated.
+		for k, v := range keys {
+			value, err := sdbv.ViewGet(view, k)
+			assert.NoError(t, err, "view failed")
+			assert.Equal(t, v, value)
+		}
+
+		// check that none of the values changed in the view
+		for k, v := range keys {
+			value, err := sdbv.ViewGet(view, k)
+			assert.NoError(t, err, "view failed")
+			assert.Equal(t, v, value)
+		}
+
+		// check that the DB state is all still good
+		for k, v := range updated {
+			value := sdbv.Get(k)
+			assert.Equal(t, v, value)
+		}
+		for k, v := range keys {
+			value := sdbv.Get(k)
+			if value2, ok := updated[k]; ok {
+				assert.Equal(t, value, value2, "updated value lost")
+			} else {
+				assert.Equal(t, value, v, "original value wrong")
+			}
+		}
+
+		time.Sleep(Timeout / 5) // sleep for a bit to allow some views to timeout
+	}
+
+	time.Sleep(Timeout * 10 / 11)
+	sdbv.IsViewActive()
+	assert.False(t, sdbv.IsViewActive(), "no view should be active")
+	assert.True(t, len(sdbv.ActiveViews) == 0, "nothing should be in ActiveViews")
+
+}
+
+
